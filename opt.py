ddc56198ee66a2db24daa261668ace2acc4c4c60
maher460 <maher460@gmail.com>
2019-04-20 21:26:54 -0400

    changed hyperparameters

--- opt.py
+++ opt.py
@@ -30,7 +30,7 @@ def default_options():
 
 
     ### MODEL CONFIG
-    options['video_feat_dim'] =  50 #500 # dim of image feature
+    options['video_feat_dim'] =  500 #500 # dim of image feature
     options['encoded_video_feat_dim'] = 32 #512 # should be equal to rnn size
     options['word_embed_size'] = 32 #512    # size of word embedding
     options['caption_seq_len'] = 11 #30  # maximu length of a sentence
